ad15c48ab65e21d873000eca251fa0a3a0a76b9d
James Smith <thisisjamessmith@gmail.com>
2019-08-23 11:10:40 +0100

    fixing og image to use an absolute URL and cleaning up general config

--- config/general.php
+++ config/general.php
@@ -13,6 +13,8 @@ return [
     // All environments
     '*' => [
         'env' => getenv('ENVIRONMENT'),
+        'securityKey' => getenv('SECURITY_KEY'),
+        'useProjectConfigFile' => true,
 
         'siteUrl' => getenv('SITE_URL'),
         // if multi-site, do it like this...
@@ -21,47 +23,56 @@ return [
         //     'siteTwoHandle' => getenv('SITE2_URL'),
         // ],
 
-        'useProjectConfigFile' => true,
         'isSystemLive' => true,
-        'cpTrigger' => 'admin',
-        'defaultWeekStartDay' => 1,
-        'cacheDuration' => false,
+        'cpTrigger' => 'admin', // change me
+
+        // fuzzy search
         'defaultSearchTermOptions' => array(
             'subLeft' => true,
             'subRight' => true,
         ),
-        'enableCsrfProtection' => true,
+
+        // prevent /cpresources/ path showing up on the front end
         'generateTransformsBeforePageLoad' => true,
+
+        // remove index.php from auto-generated URLs
         'omitScriptNameInUrls' => true,
-        'securityKey' => getenv('SECURITY_KEY'),
+
+        'sendPoweredByHeader' => false,
         'useEmailAsUsername' => true,
-        'errorTemplatePrefix' => "_errors/",
+        'errorTemplatePrefix' => '_errors/',
+        'allowUpdates' => false,
+
+        // too memory-intensive for many of our DBs,
+        // and a bit redundant given how careful we usually are...
+        'backupOnUpdate' => false,
+
+        // if multi-site and not sharing a symlinked assets path, specify them like this
+        // and then use these aliases as prefixes in the control panel (Settings > Assets > File System Path)
+        // - so site-1's images directory would be `@basePathSite1/media/images`
+        // 'aliases' => [
+        //     '@basePathSite1' => '../public_site1',
+        //     '@basePathSite2' => '../public_site2',
+        // ],
     ],
 
     // Live (production) environment
     'live' => [
-        'allowUpdates' => false,
-        'backupOnUpdate' => false,
-        'devMode' => false,
-        'enableTemplateCaching' => true,
-
     ],
 
     // dev (pre-production) environment
     'dev' => [
-        'allowUpdates' => false,
-        'backupOnUpdate' => false,
-        'devMode' => false,
-        'enableTemplateCaching' => true,
-        'userSessionDuration' => false,
+        'userSessionDuration' => 0, // disable auto-logout in the control panel
     ],
 
-    // Local (development) environment
+    // local (development) environment
     'local' => [
+        'userSessionDuration' => 0,
         'allowUpdates' => true,
-        'backupOnUpdate' => false,
         'devMode' => true,
         'enableTemplateCaching' => false,
-        'userSessionDuration' => false,
+        'testToEmailAddress' => getenv('SYSTEM_EMAIL'),
+        // this marks any strings that are translated, making it easier to spot ones which are not...
+        'translationDebugOutput' => true,
     ],
 ];
